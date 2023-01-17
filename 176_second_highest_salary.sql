23a07db17b36bd61edf9ee2edbe86364a56055e6
ic10134 <ic10134@gmail.com>
2023-01-17 17:16:53 -0500

    Day-3
    Day-3 changes

---
+++ 176_second_highest_salary.sql
@@ -0,0 +1,12 @@
+select 
+    case 
+        when  max(rank_desc) <> 1 then salary
+              else null    
+        end AS SecondHighestSalary
+from
+(select
+    salary,
+    rank() over(order by salary desc) as rank_desc
+from employee) cor_qry
+where rank_desc=2
+;
